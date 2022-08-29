921efc0d17d628ac395587f8733191009c6259b1
ZULFI <zulfiqar.siddique@gmail.com>
2022-08-29 00:14:14 -0500

    ZZZ

---
+++ src/test/java/Features/Cart2.feature
@@ -0,0 +1,10 @@
+Feature: This is a Cart functionality
+
+Scenario: Verify user item in Cart2
+Given Launch application url
+Then Click Samsungs6
+When Click AddToCart
+Then Click Cart
+Then Verify item added in Cart
+
+
---
+++ src/test/java/StepDefinition/Cart2.java
@@ -0,0 +1,36 @@
+package StepDefinition;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class Cart2 {
+
+	
+	@Given("^Launch application url$")
+	public void launch_application_url() throws Throwable {
+	
+	}
+
+	@Then("^Click Samsungs(\\d+)$")
+	public void click_Samsungs(int arg1) throws Throwable {
+	
+	}
+
+	@When("^Click AddToCart$")
+	public void click_AddToCart() throws Throwable {
+	
+	}
+
+	@Then("^Click Cart$")
+	public void click_Cart() throws Throwable {
+	
+	}
+
+	@Then("^verify item added in Cart$")
+	public void verify_item_added_in_Cart() throws Throwable {
+	
+	}
+
+	
+}
